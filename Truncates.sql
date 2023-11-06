32166dea64e9c0bbb6d03d0ca3ddbb759f298bb7
MythriRayudu <66509891+MythriRayudu@users.noreply.github.com>
2023-11-06 13:21:20 +0530

    Add files via upload

---
+++ Truncates.sql
@@ -0,0 +1,19 @@
+set foreign_key_checks = 0;
+
+set foreign_key_checks = 1;
+
+truncate table B_QUESTIONNAIRE_RESPONSE;
+truncate table J_QUESTIONNAIRE_RESPONSE_STUDY;
+truncate table J_QUESTIONNAIRE_ANSWER;
+
+
+
+truncate table B_QUESTIONNAIRE ;
+truncate table B_QUESTIONNAIRE_SECTION;
+truncate table B_QUESTIONNAIRE_INSTRUCTIONS;
+truncate table B_QUESTIONNAIRE_QUESTION;
+truncate table B_QUESTION_ANSWER_OPTION;
+truncate table J_STUDY_QUESTIONNAIRE ;
+truncate table J_STUDY_QUESTIONNAIRE_DATA_EXTRACT_CONFIG;
+truncate table J_QUESTION_ANSWER_OPTION_CATEGORY;
+truncate table J_QUESTION_CATEGORY ;
